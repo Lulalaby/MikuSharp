bc9566eac399cbdee25566776d9b3efd771f9b1f
Lala Sabathil <aiko@aitsys.dev>
2026-03-21 09:41:16 +0100

    removed translator generator

--- TranslatorGenerator/Helpers.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DisCatSharp.TranslationGenerator;
-
-internal static class Helpers
-{
-	internal static string RemoveLtfBs(this string str)
-		=> str.Replace("translation_generator_export-", "").Replace('-', ' ').Replace('_', ' ').Replace(".json", "");
-}
--- TranslatorGenerator/out/README.md
+++
@@ -1,1 +0,0 @@
-﻿# Generated translations for Miku Bot
